3bc8b25810b6175e7faf46268c90aa607df226b6
fabiolino <fabiocasilli@gmail.com>
2026-09-16 01:35:42 +0200

    Update Venues.jsx

--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -90,7 +90,7 @@ export default function Venues() {
 
             <div style={{ display: "flex", gap: 8, marginTop: 10 }}>
               {v.contact_phone && (
-                
+              <a  
                   href={`tel:${v.contact_phone}`}
                   style={{
                     flex: 1,

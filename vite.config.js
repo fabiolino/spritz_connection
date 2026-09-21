3323f85ffd125665b9dd8c0b747651ee63a69777
fabiolino <fabiocasilli@gmail.com>
2026-09-21 23:05:54 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: false,
       includeAssets: ["logo.jpg"],
       manifest: {
         name: "Spritz Connection",
